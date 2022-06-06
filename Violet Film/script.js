e33cff8dbe8b923ffe839e15defdc1b526c5f9d0
rifqiCode <54792234+cyberif@users.noreply.github.com>
2022-06-06 20:38:35 +0700

    Add files via upload

---
+++ Violet Film/script.js
@@ -0,0 +1,10 @@
+
+function loginPage() {
+    window.location.href = "#";
+    // window.location.replace("https://www.artinfo.my.id/");
+}
+
+function registrasiPage() {
+    window.location.href = "#";
+    // window.location.replace("https://www.artinfo.my.id/");
+}
